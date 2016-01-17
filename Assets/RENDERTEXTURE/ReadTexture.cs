188a63a32d7f636d42e1674078febec98bd115c3
Benjamin Ma <benjamin@ma.dk>
2016-01-17 16:40:49 +0100

    Naming Ben

--- Assets/RENDERTEXTURE/ReadTexture.cs
+++ Assets/RENDERTEXTURE/ReadTexture.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class ReadTextureBen : MonoBehaviour 
+public class ReadTexture : MonoBehaviour 
 {
     public RenderTexture renderTex;
     public Texture2D tex;
